0e4669b47d86f12a6af1dd2e709c408328331a6e
Fils25git <fleduportal25@gmail.com>
2025-03-15 11:52:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function toggleMenu() {
          menu.classList.toggle("show");
 }
    
+document.addEventListener("DOMContentLoaded", () => {
+            // Hamburger menu toggle
+            const menuButton = document.querySelector(".hamburger-menu");
+            const menu = document.getElementById("nav-links");
+
+            if (menuButton && menu) {
+                menuButton.addEventListener("click", function () {
+                    menu.classList.toggle("show");
+                });
+            }
+        });
